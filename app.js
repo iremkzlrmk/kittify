0214d8f3d1e537f984cae2b151cb1454c61b48f0
U200709063 <iremkizilirmak@posta.mu.edu.tr>
2023-01-23 19:30:08 +0300

    refactored the routes

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
 const express = require("express");
 const http = require("http");
+const fs = require("fs");
 
 const app = express();
 app.listen(3000);
+
+app.get("/text/:text_id", (req, res) => {
+
+    let text_id = req.params["text_id"];
+
+    http.get(`http://localhost:3001/getText/${text_id}`, (resp) => {
+
+        let data = '';
+        
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+        
+        resp.on('end', () => {
+            console.log(data);
+            res.send(data);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+});
+
+
+app.get("/image/:image_id", (req, res) => {
+
+    let image_id = req.params["image_id"];
+
+    http.get(`http://localhost:3001/getImage/${image_id}`, (resp) => {
+       
+        resp.setEncoding('binary');
+        let imageData = '';
+        
+        resp.on('data', (chunk) => { 
+            imageData += chunk; 
+        });
+        
+        resp.on('end', () => {
+            const buffer = Buffer.from(imageData, "binary");
+            fs.writeFileSync('image.jpg', buffer);
+            console.log('image saved.');
+            res.set('Content-Type', 'image/jpeg');
+            res.send(buffer);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+});
 
 
 app.get("/track/:track_id", (req, res) => {
@@ -10,21 +60,23 @@ app.get("/track/:track_id", (req, res) => {
     let track_id = req.params["track_id"];
 
     http.get(`http://localhost:3001/getTrack/${track_id}`, (resp) => {
-        let data = '';
+       
+        resp.setEncoding('binary');
+        let trackData = '';
         
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-            data += chunk;
+        resp.on('data', (chunk) => { 
+            trackData += chunk; 
         });
         
-        // The whole response has been received. Print out the result.
         resp.on('end', () => {
-            console.log(data);
-            res.send(data);
+            const buffer = Buffer.from(trackData, "binary");
+            fs.writeFileSync('audio.mp3', buffer);
+            console.log('track saved.');
+            res.set('Content-Type', 'audio/mpeg');
+            res.send(buffer);
         });
-        
+
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     });
-    
 });
--- serverapp.js
+++ serverapp.js
@@ -1,20 +1,61 @@
 const express = require("express");
 
 const app = express();
-
 app.listen(3001);
 
 app.get("/", (req, res) => {
     res.send("homepage of the server");
 });
 
+app.get("/getText/:text_id", (req, res) => {
+
+    if (req.params["text_id"] == 999){
+        res.send("text 999");
+        console.log("success");
+    }
+
+    if (req.params["text_id"] == 666){
+        res.send("text 666");
+        console.log("success");
+    }
+
+    else {
+        res.send("text does not exist");
+        console.log("failure");
+    }
+});
+
+
+app.get("/getImage/:image_id", (req, res) => {
+
+    if (req.params["image_id"] == 999){
+        res.sendFile("./aesth.jpg", { root: __dirname });
+        console.log("success");
+        return;
+    }
+
+    if (req.params["image_id"] == 666){
+        res.sendFile("./aesth.jpg", { root: __dirname });
+        console.log("success");
+        return;
+    }
+    
+    else {
+        res.send("image does not exist");
+        console.log("failure");
+    }
+});
+
+
 app.get("/getTrack/:track_id", (req, res) => {
 
-    if (req.params["track_id"] == 666){
-        res.send("track imported successfully");
+    if (req.params["track_id"] == 999){
+        res.sendFile("./ending_credits_meri.mp3", {root: __dirname});
         console.log("success");
-    } else {
-        res.send("song does not exist");
+    } 
+
+    else {
+        res.send("track does not exist");
         console.log("failure");
     }
 });
