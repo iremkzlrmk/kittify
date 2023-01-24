123d21489dd9d7bc8fe6ae9204505c83e64d19a6
U200709063 <iremkizilirmak@posta.mu.edu.tr>
2023-01-24 11:57:40 +0300

    handled the response formats

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.get("/text/:text_id", (req, res) => {
     http.get(`http://localhost:3001/getText/${text_id}`, (resp) => {
 
         let data = '';
-        
+
         resp.on('data', (chunk) => {
             data += chunk;
         });
-        
+
         resp.on('end', () => {
             console.log(data);
             res.send(data);
@@ -33,21 +33,41 @@ app.get("/image/:image_id", (req, res) => {
     let image_id = req.params["image_id"];
 
     http.get(`http://localhost:3001/getImage/${image_id}`, (resp) => {
-       
-        resp.setEncoding('binary');
-        let imageData = '';
-        
-        resp.on('data', (chunk) => { 
-            imageData += chunk; 
-        });
-        
-        resp.on('end', () => {
-            const buffer = Buffer.from(imageData, "binary");
-            fs.writeFileSync('image.jpg', buffer);
-            console.log('image saved.');
-            res.set('Content-Type', 'image/jpeg');
-            res.send(buffer);
-        });
+
+        let content_type = resp.headers["content-type"].split(";").at(0);
+
+        switch (content_type) {
+
+            case "text/html":
+                let data = '';
+
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                resp.on('end', () => {
+                    console.log(data);
+                    res.send(data);
+                });
+                break;
+
+            case "image/jpeg":
+                resp.setEncoding('binary');
+                let imageData = '';
+
+                resp.on('data', (chunk) => {
+                    imageData += chunk;
+                });
+
+                resp.on('end', () => {
+                    const buffer = Buffer.from(imageData, "binary");
+                    fs.writeFileSync('image.jpg', buffer);
+                    console.log('image saved.');
+                    res.set('Content-Type', 'image/jpeg');
+                    res.send(buffer);
+                });
+                break;
+        }
 
     }).on("error", (err) => {
         console.log("Error: " + err.message);
@@ -60,21 +80,41 @@ app.get("/track/:track_id", (req, res) => {
     let track_id = req.params["track_id"];
 
     http.get(`http://localhost:3001/getTrack/${track_id}`, (resp) => {
-       
-        resp.setEncoding('binary');
-        let trackData = '';
-        
-        resp.on('data', (chunk) => { 
-            trackData += chunk; 
-        });
-        
-        resp.on('end', () => {
-            const buffer = Buffer.from(trackData, "binary");
-            fs.writeFileSync('audio.mp3', buffer);
-            console.log('track saved.');
-            res.set('Content-Type', 'audio/mpeg');
-            res.send(buffer);
-        });
+
+        let content_type = resp.headers["content-type"].split(";").at(0);
+
+        switch (content_type) {
+
+            case "text/html":
+                let data = '';
+
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                resp.on('end', () => {
+                    console.log(data);
+                    res.send(data);
+                });
+                break;
+
+            case "audio/mpeg":
+                resp.setEncoding('binary');
+                let trackData = '';
+
+                resp.on('data', (chunk) => {
+                    trackData += chunk;
+                });
+
+                resp.on('end', () => {
+                    const buffer = Buffer.from(trackData, "binary");
+                    fs.writeFileSync('audio.mp3', buffer);
+                    console.log('track saved.');
+                    res.set('Content-Type', 'audio/mpeg');
+                    res.send(buffer);
+                });
+                break;
+        }
 
     }).on("error", (err) => {
         console.log("Error: " + err.message);
--- serverapp.js
+++ serverapp.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 app.listen(3001);
@@ -9,53 +10,67 @@ app.get("/", (req, res) => {
 
 app.get("/getText/:text_id", (req, res) => {
 
-    if (req.params["text_id"] == 999){
-        res.send("text 999");
-        console.log("success");
-    }
+    switch (parseInt(req.params.text_id)) {
 
-    if (req.params["text_id"] == 666){
-        res.send("text 666");
-        console.log("success");
-    }
+        case 999:
+            res.send("text 999");
+            console.log("success");
+            break;
 
-    else {
-        res.send("text does not exist");
-        console.log("failure");
+        case 666:
+            res.send("text 666");
+            console.log("success");
+            break;
+
+        default:
+            res.send("text does not exist");
+            console.log("failure");
+            break;
     }
 });
 
 
 app.get("/getImage/:image_id", (req, res) => {
 
-    if (req.params["image_id"] == 999){
-        res.sendFile("./aesth.jpg", { root: __dirname });
-        console.log("success");
-        return;
-    }
+    switch (parseInt(req.params.image_id)) {
 
-    if (req.params["image_id"] == 666){
-        res.sendFile("./aesth.jpg", { root: __dirname });
-        console.log("success");
-        return;
-    }
-    
-    else {
-        res.send("image does not exist");
-        console.log("failure");
+        case 999:
+            res.sendFile("./aesth.jpg", { root: __dirname });
+            console.log("success999");
+            break;
+
+        case 666:
+            res.sendFile("./aesth.jpg", { root: __dirname });
+            console.log("success666");
+            break;
+
+        default:
+            res.send("image does not exist");
+            console.log("failure");
+            break;
     }
 });
 
 
 app.get("/getTrack/:track_id", (req, res) => {
 
-    if (req.params["track_id"] == 999){
-        res.sendFile("./ending_credits_meri.mp3", {root: __dirname});
-        console.log("success");
-    } 
+    switch (parseInt(req.params.track_id)) {
 
-    else {
-        res.send("track does not exist");
-        console.log("failure");
+        case 999:
+            const stream = fs.createReadStream("./ending_credits_meri.mp3");
+            stream.pipe(res);
+            // res.sendFile("./ending_credits_meri.mp3", { root: __dirname });
+            console.log("success");
+            break;
+
+        case 666:
+            res.sendFile("./ending_credits_meri.mp3", { root: __dirname });
+            console.log("success");
+            break;
+
+        default:
+            res.send("track does not exist");
+            console.log("failure");
+            break;
     }
 });
