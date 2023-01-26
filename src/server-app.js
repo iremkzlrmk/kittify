0937b9c398ee787436e1affcca211c3d368ac7ef
U200709063 <iremkizilirmak@posta.mu.edu.tr>
2023-01-26 10:24:59 +0300

    embedded stream to an html file

--- src/server-app.js
+++ src/server-app.js
@@ -48,11 +48,39 @@ app.get("/images/:trackId", async (req, res) => {
 });
 
 
-// app.get("/player/:trackId", async (req, res) => {
+app.get("/player/:trackId", async (req, res) => {
 
-//     const trackId = req.params.trackId;
+    const trackId = req.params.trackId;
+    const filePath = `${audioDir}/${trackId}.mp3`;
 
-// });
+    //media player
+    var stat = fs.statSync(filePath);
+    var total = stat.size;
+    if (req.headers.range) {
+        var range = req.headers.range;
+        var parts = range.replace(/bytes=/, "").split("-");
+        var partialstart = parts[0];
+        var partialend = parts[1];
+
+        var start = parseInt(partialstart, 10);
+        var end = partialend ? parseInt(partialend, 10) : total-1;
+        var chunksize = (end-start)+1;
+        var readStream = fs.createReadStream(filePath, {start: start, end: end});
+        res.writeHead(206, {
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+            'Accept-Ranges': 'bytes', 'Content-Length': chunksize,
+            'Content-Type': 'audio/mpeg'
+        });
+        readStream.pipe(res);
+        console.log(`total: ${total}, range: ${req.headers.range}, chunksize: ${chunksize}, end: ${end},  start: ${start}`); 
+    } else {
+        res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
+        fs.createReadStream(filePath).pipe(res);
+        console.log(`total: ${total}, res: ${res}`);
+     }
+
+
+});
 
 
 
